7974434f38c7e1cd0438504d49689b2a64f80ee4
Bruhsader <53186400+RJOOJ-JS@users.noreply.github.com>
2021-12-06 16:20:59 -0400

    Now Playing Command: Also added what song comes next

--- src/commands/dj/nowplaying.ts
+++ src/commands/dj/nowplaying.ts
@@ -10,8 +10,10 @@ async function execute({message, args, client}: ICommandArgs){
     }
     const embed = new MessageEmbed();
     const song = thisQueue.songs[0];
+    const next = thisQueue.songs[1];
+    const nextLabel = (next)? `\nProxima: ${next.original_title}` : ""
     embed.setTitle("Situação atual da nossa festa");
-    embed.addField(song.original_title, `Duração: ${SecondsToFormatedTime(<number>song.duration)} \nTocado: ${SecondsToFormatedTime(song.resource.playbackDuration / 1000)}\nFaltam: ${SecondsToFormatedTime(<number>song.duration - (song.resource.playbackDuration/1000))}`);
+    embed.addField(song.original_title, `Duração: ${SecondsToFormatedTime(<number>song.duration)} \nTocado: ${SecondsToFormatedTime(song.resource.playbackDuration / 1000)}\nFaltam: ${SecondsToFormatedTime(<number>song.duration - (song.resource.playbackDuration/1000))}${nextLabel}`);
     return message.channel.send({ content: "Player status: "+String(thisQueue.audioPlayer.state.status),  embeds: [embed] });
 }
 
